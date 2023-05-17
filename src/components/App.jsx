f0f04e0a55d24a0a07fe8fa36d6b459721f97f1f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-17 23:30:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-//import { Searchbar } from './Searchbar';
+import { Searchbar } from './Searchbar';
 import { Gallery } from './ImageGallery';
 import imagesAPI from '../services/images-api';
 import { LoadMoreBtn } from './Button';
@@ -14,10 +14,53 @@ export const App = () => {
   const [error, setError] = useState(null);
   const [status, setStatus] = useState('idle');
 
+  useEffect(() => {
+    if (searchQuery === '') return;
+
+    setStatus('pending');
+    imagesAPI
+      .fetchImages(searchQuery, page)
+      .then(({ hits, total }) => {
+        setHits(prev => [...prev, ...hits]);
+        setStatus('resolved');
+        setTotal(total);
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('rejected');
+      });
+  }, [searchQuery, page]);
+
+  const formSubmitHandler = searchQuery => {
+    setSearchQuery(searchQuery);
+    setPage(1);
+    setTotal(0);
+    setError(null);
+    setHits([]);
+    setStatus('idle');
+  };
+
+  const btnClickHandler = () => {
+    setPage(prev => prev + 1);
+  };
 
   return (
     <Container>
-      <Wrap></Wrap>
+      <Searchbar onSubmit={formSubmitHandler} />
+      {status === 'idle' && <h2>Enter a search query.</h2>}
+      {status === 'pending' && <Spinner />}
+      {status === 'rejected' && <h1>{error.message}</h1>}
+      {status === 'resolved' && (
+        <Wrap>
+          {hits && hits.length === 0 && (
+            <h2>Nothing was found for this query.</h2>
+          )}
+          <Gallery hits={hits} />
+          {hits && total / 12 > page && (
+            <LoadMoreBtn onClick={btnClickHandler} />
+          )}
+        </Wrap>
+      )}
     </Container>
   );
 };
