5433f1b11e4702745bdfd6ffdb802c2d110d3d1c
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-17 18:10:50 +0300

    Create ImageGallery.styled.jsx

---
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const Wrap = styled.div`
+  text-align: center;
+`;
+
+export const GalleryList = styled.ul`
+  display: grid;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-gap: 16px;
+  margin-top: 0;
+  margin-bottom: 16px;
+  padding: 0;
+  list-style: none;
+  margin-left: auto;
+  margin-right: auto;
+`;
