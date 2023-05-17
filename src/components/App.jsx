7a4e59bb9782dc3e7adbe8b31acd4e9d78e75b2a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-17 20:49:07 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from 'react';
+//import { Searchbar } from './Searchbar';
+import { Gallery } from './ImageGallery';
 import imagesAPI from '../services/images-api';
 import { LoadMoreBtn } from './Button';
 import { Spinner } from './Loader';
 import { Container, Wrap } from './App.styled';
 
 export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [hits, setHits] = useState([]);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('idle');
+
+
   return (
     <Container>
       <Wrap></Wrap>
